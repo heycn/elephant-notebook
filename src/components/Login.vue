162203f43f8077cbaada084d2fa05c1a62250d03
heycn <x1chennan@qq.com>
2022-01-27 20:39:33 +0800

    init

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="login">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        msg: '这是登录页面'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
