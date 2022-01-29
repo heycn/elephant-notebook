b24f6057dcbf5de1e18a1ba430943b7f98aca810
heycn <x1chennan@qq.com>
2022-01-29 19:55:19 +0800

    优化侧边栏

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,12 +3,12 @@
 <template>
   <div id="sidebar">
     <div class="icons">
-      <router-link to="/note/1" title="笔记"> <i class="iconfont icon-note"></i> </router-link>
-      <router-link to="/notebooks" title="笔记本"> <i class="iconfont icon-notebook"></i> </router-link>
+      <router-link to="/note/1" title="笔记本"> <i class="iconfont icon-note"></i> </router-link>
+      <router-link to="/notebooks" title="列表页"> <i class="iconfont icon-notebook"></i> </router-link>
       <router-link to="/trash/2" title="回收站"> <i class="iconfont icon-trash"></i> </router-link>
     </div>
     <div class="logout" @click="onLogout">
-      <i class="iconfont icon-logout"></i>
+      <i class="iconfont icon-logout" title="注销"></i>
     </div>
   </div>
 </template>
