e4d9edb5e5d021b59d1be628ed4d0ba30803ccec
heycn <x1chennan@qq.com>
2022-01-30 01:09:59 +0800

    完成登录界面基本交交互

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,19 +8,20 @@
           <div class="main"></div>
           <div class="form">
             <div class="loginAndRegister">
-              <h3 title="还没有账户？3步就能创建哦~">创建账户</h3>
-              <h3 title="已有账户？现在登录吧!">立即登录</h3>
+              <h3 @click="showRegister" title="还没有账户？3步就能创建哦~">创建账户</h3>
+              <h3 @click="showLogin" title="已有账户？现在登录吧!">立即登录</h3>
             </div>
-            <div v-show="true" class="register">
-              <input type="text" placeholder="用户名" />
-              <input type="password" placeholder="密码" />
-              <div class="button" title="最后一步，点击创建!">创建</div>
+            <div v-show="isShowRegister" class="register">
+              <input type="text" v-model="register.username" placeholder="用户名" />
+              <input type="password" v-model="register.password" placeholder="密码" />
+              <p v-bind:class="{ error: register.isError }">{{ register.notice }}</p>
+              <div class="button" @click="onRegister" title="最后一步，点击创建!">创建</div>
             </div>
-
-            <div v-show="false" class="login">
-              <input type="text" placeholder="输入用户名" />
-              <input type="password" placeholder="密码" />
-              <div class="button" title="点击登录">登录</div>
+            <div v-show="isShowLogin" class="login">
+              <input type="text" v-model="login.username" placeholder="输入用户名" />
+              <input type="password" v-model="login.password" placeholder="密码" />
+              <p v-bind:class="{ error: login.isError }">{{ login.notice }}</p>
+              <div class="button" @click="onLogin" title="点击登录">登录</div>
             </div>
           </div>
         </div>
@@ -30,13 +31,89 @@
 </template>
 
 <script>
-  import NoteDetail from './NoteDetail.vue'
   export default {
     name: 'Login',
-    components: { NoteDetail }
+    data() {
+      return {
+        isShowLogin: true,
+        isShowRegister: false,
+        login: {
+          username: '',
+          password: '',
+          notice: '请输入用户名和密码',
+          isError: false // isError 是来控制 notice 是否展示 默认隐藏
+        },
+        register: {
+          username: '',
+          password: '',
+          notice: '创建账号后，要记住用户名和密码哦~',
+          isError: false // 和 login 里一样 控制 notice 是否展示
+        }
+      }
+    },
+    methods: {
+      showRegister() {
+        this.isShowRegister = true
+        this.isShowLogin = false
+      },
+      showLogin() {
+        this.isShowLogin = true
+        this.isShowRegister = false
+      },
+      onRegister() {
+        let result1 = this.validUsername(this.register.username)
+        if (!result1.isValid) {
+          this.register.isError = true
+          this.register.notice = result1.notice
+          return
+        }
+        let result2 = this.validPassword(this.register.password)
+        if (!result2.isValid) {
+          this.register.isError = true
+          this.register.notice = result2.notice
+          return
+        }
+        // TODO
+        // 等接口写好之后再写提交
+        this.register.isError = false
+        this.register.notice = ''
+        console.log('别tm再点提交啦!用户注册的接口还写好啊!')
+      },
+      onLogin() {
+        let result1 = this.validUsername(this.login.username)
+        if (!result1.isValid) {
+          this.login.isError = true
+          this.login.notice = result1.notice
+          return
+        }
+        let result2 = this.validPassword(this.login.password)
+        if (!result2.isValid) {
+          this.login.isError = true
+          this.login.notice = result2.notice
+          return
+        }
+        // TODO
+        // 等接口写好之后再写提交
+        this.login.isError = false
+        this.login.notice = ''
+        console.log('别tm再点啦!接口还写好啊!')
+      },
+      validUsername(username) {
+        return {
+          isValid: /^[a-zA-Z_0-9\u4e00-\u9fa5]{3,15}$/.test(username),
+          notice: '用户名必须是3~15个字符，且限于字母数字中文下划线'
+        }
+      },
+      validPassword(password) {
+        return {
+          isValid: /^.{6,16}$/.test(password),
+          notice: '密码长度必须为6~16个字符'
+        }
+      }
+    }
   }
 </script>
-<style lang="less">
+<style lang="less" scoped>
   .modal-mask {
     position: fixed;
     z-index: 100;
@@ -106,7 +183,7 @@
         height: 36px;
         line-height: 36px;
         text-align: center;
-        font-weight: bold;
+        font-weight: 600;
         color: #fff;
         border-radius: 4px;
         margin-top: 18px;
