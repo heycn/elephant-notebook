59184fcd583ed4c4cd09c8bc4225180dfc138877
heycn <x1chennan@qq.com>
2022-01-29 12:50:49 +0800

    create Sidebar.vue html css

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="sidebar">
+    <div class="icons">
+      <router-link to="/note/1" title="笔记"> <i class="iconfont icon-note"></i> </router-link>
+      <router-link to="/notebooks" title="笔记本"> <i class="iconfont icon-notebook"></i> </router-link>
+      <router-link to="/trash/2" title="回收站"> <i class="iconfont icon-trash"></i> </router-link>
+    </div>
+    <div class="logout" @click="onLogout">
+      <i class="iconfont icon-logout"></i>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #sidebar {
+    position: relative;
+    max-width: 56px;
+    text-align: center;
+    background: #353a5a;
+  }
+
+  .icons {
+    margin-top: 16px;
+  }
+
+  .icons a {
+    padding: 6px 0;
+    display: block;
+  }
+
+  .icons .router-link-active {
+    background: #4a4e6b;
+  }
+
+  .logout {
+    position: absolute;
+    bottom: 20px;
+    widows: 100%;
+    text-align: center;
+    cursor: pointer;
+  }
+
+  .iconfont {
+    color: #fff;
+  }
+</style>
