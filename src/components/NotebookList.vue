7fd69001db43ae5d61156b97f1d1533b0231eafe
heycn <x1chennan@qq.com>
2022-02-02 12:06:02 +0800

    渲染笔记本列表

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -7,28 +7,18 @@
     </header>
     <main>
       <div class="layout">
-        <h3>笔记本列表(10)</h3>
+        <h3>笔记本列表({{ notebooks.length }})</h3>
         <div class="book-list">
-          <a href="#" class="notebook">
+          <router-link v-for="notebook in notebooks" to="/note/1" class="notebook">
             <div>
               <span class="iconfont icon-notebook"></span>
-              笔记本标题1
-              <span>3</span>
-              <span class="action" @click="onEdit">编辑</span>
-              <span class="action" @click="onDelete">删除</span>
-              <span class="date">3天前</span>
-            </div>
-          </a>
-          <a href="#" class="notebook">
-            <div>
-              <span class="iconfont icon-notebook"></span>
-              笔记本标题1
-              <span>1</span>
+              {{ notebook.title }}
+              <span>{{ notebook.noteCounts }}</span>
               <span class="action">编辑</span>
               <span class="action">删除</span>
-              <span class="date">5天前</span>
+              <span class="date">3天前</span>
             </div>
-          </a>
+          </router-link>
         </div>
       </div>
     </main>
@@ -37,6 +27,7 @@
 
 <script>
   import Auth from '@/apis/auth'
+  import Notebooks from '@/apis/notebooks'
 
   export default {
     data() {
@@ -50,6 +41,10 @@
         if (!res.isLogin) {
           this.$router.push({ path: '/login' })
         }
+        this.notebooks = res.data
+      })
+
+      Notebooks.getAll().then(res => {
         this.notebooks = res.data
       })
     },
