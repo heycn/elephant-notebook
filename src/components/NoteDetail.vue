7db690800db600c2cc33fbcdcc7e2f17dd2161ee
heycn <x1chennan@qq.com>
2022-02-05 16:49:18 +0800

    笔记详情页编辑区结构样式

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -4,34 +4,19 @@
   <div id="note" class="detail">
     <NoteSidebar />
     <div class="note-detail">
-      <div class="note-empty" v-show="!curNote.id">请选择笔记</div>
-      <div class="note-detail-ct" v-show="curNote.id">
-        <div class="note-bar">
-          <span> 创建日期: {{ curNote.createdAtFriendly }}</span>
-          <span> 更新日期: {{ curNote.updatedAtFriendly }}</span>
-          <span> {{ statusText }}</span>
-          <span class="iconfont icon-delete" @click="deleteNote"></span>
-          <span class="iconfont icon-fullscreen" @click="isShowPreview = !isShowPreview"></span>
-        </div>
-        <div class="note-title">
-          <input
-            type="text"
-            v-model:value="curNote.title"
-            @input="updateNote"
-            @keydown="statusText = '正在输入...'"
-            placeholder="输入标题"
-          />
-        </div>
-        <div class="editor">
-          <textarea
-            v-show="isShowPreview"
-            v-model:value="curNote.content"
-            @input="updateNote"
-            @keydown="statusText = '正在输入...'"
-            placeholder="输入内容, 支持 markdown 语法"
-          ></textarea>
-          <div class="preview markdown-body" v-html="previewContent" v-show="!isShowPreview"></div>
-        </div>
+      <div class="note-bar">
+        <span> 创建日期：{{ curNote.createdAtFriendly }}</span>
+        <span> 更新日期：{{ curNote.updatedAtFriendly }}</span>
+        <span> {{ curNote.statusText }} </span>
+        <span class="iconfont icon-delete"></span>
+        <span class="iconfont icon-fullscreen"></span>
+      </div>
+      <div class="note-title">
+        <input type="text" :value="curNote.content" placeholder="输入标题" />
+      </div>
+      <div class="editor">
+        <textarea v-show="true" :value="curNote.content" placeholder="输入内容，支持 Markdown 语法"></textarea>
+        <div class="preview markdown-body" v-html="" v-show="false"></div>
       </div>
     </div>
   </div>
@@ -48,7 +33,13 @@
 
     data() {
       return {
-        msg: '笔记本详情列表'
+        curNote: {
+          title: '我的笔记',
+          content: '我的笔记内容',
+          createdAtFriendly: '1天前',
+          updateAtFriendly: '刚刚',
+          statusText: '未更新'
+        }
       }
     },
 
