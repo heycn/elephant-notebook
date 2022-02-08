03a4f82363db2ecaadc886280c869d26ecd35c3b
heycn <x1chennan@qq.com>
2022-02-08 21:30:29 +0800

    vuex trash 模块的完善 未 diff

--- src/components/TrashDetail.vue
+++ src/components/TrashDetail.vue
@@ -38,65 +38,49 @@
 </template>
 
 <script>
-  import Auth from '@/apis/auth'
   import MarkdownIt from 'markdown-it'
-  import Trash from '@/apis/trash'
-
-  window.Trash = Trash
+  import { mapGetters, mapMutations, mapActions } from 'vuex'
 
   let md = new MarkdownIt()
 
   export default {
     data() {
       return {
-        msg: '回收站',
-        curTrashNote: {
-          id: 1,
-          title: '我的笔记',
-          content: '## hello',
-          createdAtFriendly: '2小时前',
-          updatedAtFriendly: '刚刚'
-        },
-        belongTo: '我的笔记本',
-        trashNotes: [
-          {
-            id: 2,
-            title: '我的笔记',
-            content: '## hello',
-            createdAtFriendly: '2小时前',
-            updatedAtFriendly: '刚刚'
-          },
-          {
-            id: 3,
-            title: '我的笔记',
-            content: '## hello',
-            createdAtFriendly: '2小时前',
-            updatedAtFriendly: '刚刚'
-          }
-        ]
+        belongTo: '我的笔记本'
       }
     },
 
     created() {
-      // Auth.getInfo().then(res => {
-      //   if (!res.isLogin) {
-      //     this.$router.push({ path: '/login' })
-      //   }
-      // })
+      this.checkLogin({ path: '/login' })
+      this.getTrashNotes().then(() => {
+        this.setCurTrashNote({ curTrashNoteId: this.$route.query.noteId })
+      })
     },
 
     computed: {
+      ...mapGetters(['trashNotes', 'curTrashNote', 'belongTo']),
+
       compiledMarkdown() {
         return md.render(this.curTrashNote.content || '')
       }
     },
 
     methods: {
+      ...mapMutations(['setCurTrashNote']),
+
+      ...mapActions(['checkLogin', 'deleteTrashNote', 'revertTrashNote', 'getTrashNotes']),
+
       onDelete() {
-        console.log('删除')
+        this.deleteTrashNote({ noteId: this.curTrashNote.id })
       },
+
       onRevert() {
-        console.log('恢复')
+        this.revertTrashNote({ noteId: this.curTrashNote.id })
+      },
+
+      beforeRouteUpdate(to, from, next) {
+        this.setCurTrashNote({ curTrashNoteId: to.query.noteId })
+        next()
       }
     }
   }
