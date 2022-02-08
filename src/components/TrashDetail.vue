42ea13e4c33a708aa76f60efc9305113fdd4f78a
heycn <x1chennan@qq.com>
2022-02-08 20:11:15 +0800

    回收站模板样式

--- src/components/TrashDetail.vue
+++ src/components/TrashDetail.vue
@@ -1,33 +1,121 @@
 <!-- 回收站详情页 -->
 
 <template>
-  <div id="trash-detail">
-    <h1>noteId : {{ $route.query.noteId }}</h1>
+  <div id="trash" class="detail">
+    <div class="note-sidebar">
+      <h3 class="notebook-title">回收站</h3>
+      <div class="menu">
+        <div>更新时间</div>
+        <div>标题</div>
+      </div>
+      <ul class="notes">
+        <li v-for="note in trashNotes">
+          <router-link :to="`/trash?noteId=${note.id}`">
+            <span class="date">{{ note.updatedAtFriendly }}</span>
+            <span class="title">{{ note.title }}</span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="note-detail">
+      <div class="note-bar" v-if="true"></div>
+      <span>创建日期：{{ curTrashNote.createdAtFriendly }}</span>
+      <span> | </span>
+      <span>创建日期：{{ curTrashNote.updatedAtFriendly }}</span>
+      <span> | </span>
+      <span>所属笔记本：{{ belongTo }}</span>
+      <a class="btn action" @click="onRevert">恢复</a>
+      <a class="btn action" @click="onDelete">彻底删除</a>
+      <div class="note-title">
+        <span>{{ curTrashNote.title }}</span>
+      </div>
+      <div class="editor">
+        <div class="preview markdown-body" v-html="compiledMarkdown"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import Auth from '@/apis/auth'
+  import MarkdownIt from 'markdown-it'
+
+  let md = new MarkdownIt()
 
   export default {
     data() {
       return {
-        msg: '回收站'
+        msg: '回收站',
+        curTrashNote: {
+          id: 1,
+          title: '我的笔记',
+          content: '## hello',
+          createdAtFriendly: '2小时前',
+          updatedAtFriendly: '刚刚'
+        },
+        belongTo: '我的笔记本',
+        trashNotes: [
+          {
+            id: 2,
+            title: '我的笔记',
+            content: '## hello',
+            createdAtFriendly: '2小时前',
+            updatedAtFriendly: '刚刚'
+          },
+          {
+            id: 3,
+            title: '我的笔记',
+            content: '## hello',
+            createdAtFriendly: '2小时前',
+            updatedAtFriendly: '刚刚'
+          }
+        ]
       }
     },
 
     created() {
-      Auth.getInfo().then(res => {
-        if (!res.isLogin) {
-          this.$router.push({ path: '/login' })
-        }
-      })
+      // Auth.getInfo().then(res => {
+      //   if (!res.isLogin) {
+      //     this.$router.push({ path: '/login' })
+      //   }
+      // })
+    },
+
+    computed: {
+      compiledMarkdown() {
+        return md.render(this.curTrashNote.content || '')
+      }
+    },
+
+    methods: {
+      onDelete() {
+        console.log('删除')
+      },
+      onRevert() {
+        console.log('恢复')
+      }
     }
   }
 </script>
 
-<style scoped>
-  h1 {
-    color: blue;
+<style lang="less" scoped>
+  @import url(../assets/css/note-sidebar.less);
+  @import url(../assets/css/note-detail.less);
+
+  #trash {
+    display: flex;
+    align-items: stretch;
+    background: #fff;
+    flex: 1;
+
+    .note-bar {
+      .action {
+        border: 1px solid red;
+        float: right;
+        margin-left: 10px;
+        padding: 2px 4px;
+        font-size: 12px;
+      }
+    }
   }
 </style>
