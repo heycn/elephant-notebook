995b3dca08da25accbe8214b0c94ab1629fb7db8
heycn <x1chennan@qq.com>
2022-02-09 21:05:36 +0800

    懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/Login'
-import Notebooks from '@/components/NotebookList'
-import NoteDetail from '@/components/NoteDetail'
-import TrashDetail from '@/components/TrashDetail'
 
 Vue.use(Router)
 
@@ -11,23 +7,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: Login
+      alias: '/notebooks',
+      component: () => import('@/components/NotebookList.vue')
     },
     {
       path: '/login',
-      component: Login
-    },
-    {
-      path: '/notebooks',
-      component: Notebooks
+      component: () => import('@/components/Login.vue')
     },
     {
       path: '/note',
-      component: NoteDetail
+      component: () => import('@/components/NoteDetail.vue')
     },
     {
       path: '/trash',
-      component: TrashDetail
+      component: () => import('@/components/TrashDetail.vue')
     }
   ]
 })
